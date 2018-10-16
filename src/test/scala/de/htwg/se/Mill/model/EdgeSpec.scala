fbefc27190ff22ab3f55616e1304010bdbceb054
Anton Hummel <anton@hummel.li>
2018-10-16 15:48:44 +0200

    Mill-1-EdgeSpec to String optimized

--- src/test/scala/de/htwg/se/Mill/model/EdgeSpec.scala
+++ src/test/scala/de/htwg/se/Mill/model/EdgeSpec.scala
@@ -4,13 +4,15 @@ import org.scalatest.{Matchers, WordSpec}
 
 class EdgeSpec extends WordSpec with Matchers {
   "A Edge" when { "new" should {
-    val edge = new Edge(1, 1)
-    "have a src and a target"  in {
+    val edge = new Edge(1, 2)
+    "has a src"  in {
       edge.src() should be(1)
-      edge.tar() should be(1)
+    }
+    "has a target" in {
+      edge.tar() should be(2)
     }
     "have a nice String representation" in {
-      edge.toString should be("src:1 - tar:1")
+      edge.toString should be("src:1 - tar:2")
     }
   }}
 }
