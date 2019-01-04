5b85121a64630097e488eefb0b778c534c31f290
Matthias Asche <matthiaskg.asche@gmail.com>
2019-01-04 11:07:43 +0100

    added some tests

--- src/test/scala/de/htwg/se/NineMensMorris/controller/Controller.scala
+++ src/test/scala/de/htwg/se/NineMensMorris/controller/Controller.scala
@@ -30,14 +30,27 @@ class Controller extends WordSpec with Matchers {
         playerOnTurn should be(playerWhite)
       }
       "have a String representation" in {
-        controller.gameboardToString should be(gameboard.toString())
+        controller.gameboardToString should be(
+          "O__________O__________O\n" +
+            "|          |          |\n" +
+            "|   O______O______O   |\n" +
+            "|   |      |      |   |\n" +
+            "|   |   O__O__O   |   |\n" +
+            "|   |   |     |   |   |\n" +
+            "O___O___O     O___O___O\n" +
+            "|   |   |     |   |   |\n" +
+            "|   |   O__O__O   |   |\n" +
+            "|   |      |      |   |\n" +
+            "|   O______O______O   |\n" +
+            "|          |          |\n" +
+            "O__________O__________O\n")
       }
 
       "get Players" in {
         controller.getPlayerOnTurn should be(controller.playerOnTurn.toString)
+
         controller.getPlayerOnTurnPhase should be(PlayerGamePhase.Place.toString)
-        controller.endPlayersTurn()
-        controller.changePlayerOnTurn()
+
 
       }
 
@@ -54,14 +67,28 @@ class Controller extends WordSpec with Matchers {
       "perform Turns" in {
         controller.placeMan(0)
         controller.placeMan(3)
+        controller.placeMan(0) should be(Error.FieldError)
         playerTest = playerTest.changeGamePhase(PlayerGamePhase.Fly)
-        controller.moveMan(0,1) should be (Error.NoError)
-        controller.moveMan(0,0) should be (Error.FieldError)
+        controller.moveMan(0, 1) should be(Error.NoError)
+        controller.moveMan(0, 0) should be(Error.FieldError)
 
-        controller.flyMan(1,5) should be (Error.NoError)
-        controller.flyMan(1,1) should be (Error.FieldError)
-        controller.performTurn(0,1) should be (Error.NoError)
+        controller.flyMan(1, 5) should be(Error.NoError)
+        controller.flyMan(1, 1) should be(Error.FieldError)
+        controller.performTurn(0, 1) should be(Error.NoError)
 
+
+      }
+
+      "change Player on Turn" in {
+        playerOnTurn = playerWhite
+
+        controller.changePlayerOnTurn()
+
+        playerOnTurn should be(playerBlack)
+
+        controller.endPlayersTurn() should be(())
+
+        playerOnTurn should be(playerWhite)
       }
     }
   }
