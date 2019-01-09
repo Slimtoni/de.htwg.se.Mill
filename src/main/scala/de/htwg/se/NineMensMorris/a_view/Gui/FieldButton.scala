769e6a3c0c067b3ef4150c1b1ad1b5cb9322ac1e
Anton Hummel <anton@hummel.li>
2019-01-09 12:50:12 +0100

    Mill19 - all Buttons inserted

--- src/main/scala/de/htwg/se/NineMensMorris/a_view/Gui/FieldButton.scala
+++ src/main/scala/de/htwg/se/NineMensMorris/a_view/Gui/FieldButton.scala
@@ -5,15 +5,21 @@ import java.io.File
 
 import javax.imageio.ImageIO
 
-import scala.swing.{Button, Graphics2D}
+import scala.swing.event.Event
+import scala.swing.{Button, Dimension, Graphics2D, Point}
 
-class FieldButton extends Button{
+case class FieldButton(id: Int) extends Button{
   val imageBlack: BufferedImage = ImageIO.read(new File("res/Black_50.png"))
   val imageWhite: BufferedImage = ImageIO.read(new File("res/Black_50.png"))
   var currentImage: BufferedImage = _
   borderPainted = false
+  minimumSize =new Dimension(50,50)
+  preferredSize = new Dimension(50,50)
+  maximumSize = new Dimension(50,50)
+
+  override def publish(e: Event): Unit = super.publish(e)
 
   override def paintComponent(g: Graphics2D): Unit = {
-    g.drawImage(imageBlack, 0, 0, null)
+    g.drawImage(imageBlack, 10, 10, null)
   }
 }
--- src/main/scala/de/htwg/se/NineMensMorris/a_view/Gui/SwingGui.scala
+++ src/main/scala/de/htwg/se/NineMensMorris/a_view/Gui/SwingGui.scala
@@ -13,36 +13,91 @@ class SwingGui(controller: ControllerInterface) extends Frame {
   visible = true
   minimumSize = new Dimension(700, 728)
   val chooseFileButton = new Button("Choose file")
-  var fieldButs: mutable.MutableList[FieldButton] = _
+  var fieldButs: mutable.MutableList[FieldButton] = mutable.MutableList.empty
   val blackIcon: Icon = new ImageIcon("res/Black_50.png")
-  for (_ <- 0 to 23) {
-    fieldButs.+=(new FieldButton)
+  for (i <- 0 to 23) {
+    val fieldtmp = FieldButton(i)
+    println(fieldtmp)
+    listenTo(fieldtmp)
+    if (fieldtmp != null) fieldButs.+=(fieldtmp)
   }
-  val dummyBut: Button = new Button {
+  //val field: FieldButton = new FieldButton
+  val dummyBut: FieldButton = new FieldButton(100) {
     visible = false
   }
 
-  listenTo(fieldButs(0))
-  listenTo(fieldButs(1))
+  //listenTo(fieldButs(1))
   //val fields =
 
   contents = new BoardPanel(controller, 7, 7) {
+
+    contents += new RowBoardPanel(1,7) {
+      contents += fieldButs.head
+      contents += dummyBut
+      contents += dummyBut
+      contents += fieldButs(1)
+      contents += dummyBut
+      contents += dummyBut
+      contents += fieldButs(2)
+    }
+    contents += new RowBoardPanel(1,7) {
+      contents += dummyBut
+      contents += fieldButs(3)
+      contents += dummyBut
+      contents += fieldButs(4)
+      contents += dummyBut
+      contents += fieldButs(5)
+      contents += dummyBut
+    }
+    contents += new RowBoardPanel(1,3) {
+      contents += fieldButs(6)
+      contents += fieldButs(7)
+      contents += fieldButs(8)
+    }
+    contents += new RowBoardPanel(1,6) {
+      contents += fieldButs(12)
+      contents += fieldButs(13)
+      contents += fieldButs(14)
+      contents += fieldButs(15)
+      contents += fieldButs(16)
+      contents += fieldButs(17)
+    }
+    contents += new RowBoardPanel(1,3) {
+      contents += fieldButs(9)
+      contents += fieldButs(10)
+      contents += fieldButs(11)
+    }
+    contents += new RowBoardPanel(1,3) {
+      contents += fieldButs(18)
+      contents += fieldButs(19)
+      contents += fieldButs(20)
+    }
+    contents += new RowBoardPanel(1,3) {
+      contents += fieldButs(21)
+      contents += fieldButs(22)
+      contents += fieldButs(23)
+    }
+    //for (fieldB <- fieldButs)
+     // contents += fieldB
     //contents.update(0,f0But)
     //contents.update(1,f1But)
-    contents += fieldButs(0)
+    //contents += field
+    /*contents += fieldButs(0)
     contents += dummyBut
     contents += dummyBut
     contents += fieldButs(1)
     contents += dummyBut
     contents += dummyBut
     contents += fieldButs(2)
-    contents += dummyBut
+    contents += dummyBut*/
 
   }
 
   reactions += {
-    case ButtonClicked(_) => {
-      Console.println("zegfzewgzi")
+    case ButtonClicked(buttonPressed) => {
+      for (i <- fieldButs)
+        if(buttonPressed == i) println(i.id)
+      //Console.println("zegfzewgzi")
     }
   }
 
