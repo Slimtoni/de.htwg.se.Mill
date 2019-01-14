a6f6539a9e2cf7727b3f05f5fdafe14b7a6fda34
Slimtoni <anton@hummel.li>
2019-01-14 17:17:24 +0100

    reset

--- src/main/scala/de/htwg/se/Mill/model/Edge.scala
+++
@@ -1,9 +0,0 @@
-package de.htwg.se.Mill.model
-
-class Edge[V](source: V, target: V) {
-
-  def src() = source
-  def tar() = target
-
-  override def toString: String = "src:" + source.toString + " - tar:" + target.toString
-}
